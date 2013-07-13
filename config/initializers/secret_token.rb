2555fddf872cffefac5a465fc641ff225994bef0
Youssif <abdulyoussif@gmail.com>
2013-07-13 09:13:12 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BathroomStall::Application.config.secret_token = '82006128add8c78c96336fa6d87b1cf9418ca8993ada4a251ad891c98241ffe9758e6ffdd14e0fed6d9d6755c155e35eb2d229905d00cdc08f305cbd0cf491a9'
