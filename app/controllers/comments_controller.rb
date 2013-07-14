80e8070f862704eceb0f93b67a91b941b73f63a7
Youssif <abdulyoussif@gmail.com>
2013-07-14 16:42:17 -0700

    pre merge

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,30 @@
+class CommentsController < ApplicationController
+	before_filter :redirect_back_unless_logged_in
+
+  def create
+  	# @user_id	= Rack.find(params[:user_id]
+		@comment = Comment.create(params[:comment]) do |comment|
+			comment.user_id = session[:user_id]
+			comment.victim_id = params[:victim_id]
+      comment.good_bad = params[:good_bad]
+      comment.anonymous = params[:anonymous]
+      # comment.name = User.find
+
+			# comment.save
+		end
+
+
+
+		redirect_to :back
+	end
+
+
+  private
+
+	def redirect_back_unless_logged_in
+		unless session[:user_id].present?
+			flash[:notice] = "Please sign in to make a comment."
+			redirect_to :back
+			end
+	end
+end
