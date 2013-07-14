39911f7c1df7115c9501fe0d22641defc233856a
Youssif <abdulyoussif@gmail.com>
2013-07-14 07:07:12 -0700

    added links and comment to search ruby gem

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 	before_filter :redirect_back_unless_logged_in
 
   def create
-  		@comment = Comment.find(params[:id])
+  		@comment = Comment.find(params[:id]).merge(user_id: params[:user_id])
   		@current_user = @comment.user_id
 		@comment = @current_user.comments.create(params[:comment]).merge(victim_id: params[:victim_id])
 		redirect_to :back
