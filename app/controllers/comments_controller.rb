42d79d0e0dd02b5084f86930baec028bb5e28d9d
Youssif <abdulyoussif@gmail.com>
2013-07-14 13:15:17 -0700

    Added anonymity and thumbs up or down parameters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,8 @@ class CommentsController < ApplicationController
 		@comment = Comment.create(params[:comment]) do |comment|
 			comment.user_id = session[:user_id]
 			comment.victim_id = params[:victim_id]
+      comment.good_bad = params[:good_bad]
+      comment.anonymous = params[:anonymous]
 
 			# comment.save
 		end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130714053158) do
+ActiveRecord::Schema.define(:version => 20130714195334) do
 
   create_table "comments", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(:version => 20130714053158) do
     t.text     "message"
     t.integer  "user_id"
     t.integer  "victim_id"
+    t.string   "good_bad"
+    t.string   "anonymous"
   end
 
   create_table "users", :force => true do |t|
